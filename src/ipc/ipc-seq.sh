ac7838d8a315b91db10a3a646d9ce7ab363cb6c1
sweatpantsdanny <d.hamilton@ufl.edu>
2019-06-02 02:20:19 -0400

    run either seq. or con. tests of all ipc's

---
+++ src/ipc/ipc-seq.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# SUMMARY
+# This script tests if the ipc module works for sequential
+# writes then reads.
+
+# EXECUTING
+# in command line, type the following:
+# ./ipc-seq.sh
+
+# give permissions for the script to run on command line
+chmod +x ipc-seq.sh
+
+# +----------+----------+----------+----------+
+# |	    MULTITHREAD SEQUENTIAL METHOD         |
+# +----------+----------+----------+----------+
+echo "RUNNING SEQUENTIAL MULTITHREAD VERSION"
+
+# add all background processes here
+python3 writer.py -multithread -seq &
+python3 reader.py -multithread -seq &
+
+exit
