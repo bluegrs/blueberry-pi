a51605b0a0a585192bea8807032dca537defa8d6
sweatpantsdanny <d.hamilton@ufl.edu>
2019-06-12 02:06:57 -0400

    search for device and receive sensor data

--- src/drivers.py
+++ src/drivers.py
@@ -6,30 +6,61 @@ asks to receive sensor data from the controller.
 
 import sys
 import config as cfg
-from wifi.sb_client import wifi_client as client
+from wifi.sb_client import client
 
 # +----------+----------+----------+----------+
 # |            MAIN (NON-THREADED)            |
 # +----------+----------+----------+----------+
 def main_single():
-    print("Running Blueberry-Pi (NON-THREADED).")
+    print("Running device drivers (NON-THREADED).")
+    connOpen = False
     
-    wifi = client(cfg.WIFI_PORT)
-    connOpen = True
+    # Try to connect to the device. If no device was found,
+    # ask the user to try again or exit.
+    while True:
+        try:
+            wifi = client(cfg.WIFI_PORT, cfg.TIMEOUT)
+            
+            # Try to receive data from the client. If there is an
+            # error, no connection was made and the exception will
+            # be thrown.
+            trash = wifi.request(cfg.ACCEL)
+            connOpen = True
+            break
+            
+        except:
+            wifi.close()
+            restart = input("No connection made. Try again? [y/n]: ")
+        
+            if restart == "y":
+                continue
+            else:
+                break
 
-    # Always listen for a connection to the client.
-    while connOpen:
-        message = 'READ'
-        connOpen = wifi.DataTxToHost(message)
+    # Only begin sending requests if a connection was made
+    if connOpen:
+        while True:
+        
+            # Request sensor data
+            ax,ay,az = wifi.request(cfg.ACCEL)
+            # print(str(ax) + " " + str(ay) + " " + str(az))
+            
+            gx,gy,gz = wifi.request(cfg.GYRO)
+            # print(str(gx) + " " + str(gy) + " " + str(gz))
+            
+            mx,my,mz = wifi.request(cfg.MAG)
+            # print(str(mx) + " " + str(my) + " " + str(mz))
 
-    print("Connection to server closed.")
+        print("Connection to server closed.")
+    
+    print("EOP.")
         
         
 # +----------+----------+----------+----------+
 # |               MAIN (THREADED)             |
 # +----------+----------+----------+----------+
 def main_threaded():
-    print("Running Blueberry-Pi (THREADED).")
+    print("Running device drivers (THREADED).")
 
 if __name__ == '__main__':
     
